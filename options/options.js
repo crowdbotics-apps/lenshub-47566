ecc076947ae5bc4053948ec3737bcbfc7de436d8
Crowdbotics <team@crowdbotics.com>
2024-04-17 13:21:24 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lenshub-47566",
+  url: "https://lenshub-47566.botics.co",
+  api: "https://lenshub-47566.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
